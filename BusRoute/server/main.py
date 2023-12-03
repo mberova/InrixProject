ce414db99438e13c2f099fc72e1804f98c104bb8
hamsterie <berovamv@gmail.com>
2023-12-02 17:26:59 -0800

    add

---
+++ BusRoute/server/main.py
@@ -0,0 +1,8 @@
+class CongestionList:
+    list = None # should be an ORDERED list of most congested areas from API
+
+    curCongestion = 0 # index of 
+    def getNextCongestion():
+
+def getCongestionList(rad: number, c: center):
+    # return a ORDERED list of congested areas within the given circle using API
